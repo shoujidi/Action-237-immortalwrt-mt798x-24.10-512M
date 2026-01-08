f3df76bbd7a7f306424cea003685665e6e26a192
null <68135821+shoujidi@users.noreply.github.com>
2026-01-08 16:01:21 +0800

    Update diy-part1.sh
    原脚本中 Passwall 地址缺少 ;main 分支标识，且顺序在后。根据原作者提醒，应将 Passwall 置于 feeds.conf.default 的最顶端

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,12 +10,12 @@
 # See /LICENSE for more information.
 #
 
-# 1. 添加 OpenClash 官方源
+# 1. 按照原作者要求，在 feeds 第一行插入最新的 Passwall 源码
+sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+
+# 2. 添加 OpenClash 官方源
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 
-# 2. (可选) 添加 Passwall 源（如果你也想尝试这个的话）
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-
-# 3. 集成常用依赖包源
+# 3. 集成常用依赖包源 (备用)
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
