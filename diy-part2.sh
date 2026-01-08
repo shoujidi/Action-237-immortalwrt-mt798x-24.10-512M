f31f5da118c2649d3db6330dfc35181924a3b062
null <68135821+shoujidi@users.noreply.github.com>
2026-01-08 15:19:41 +0800

    Update diy-part2.sh
    更新512M

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,14 +10,23 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 修改默认管理 IP 为 192.168.0.1
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. 修改默认主机名为 360-T7-512M (可选，方便识别)
+sed -i 's/OpenWrt/360-T7-512M/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# --- 512M Flash 适配开始 ---
 
-#rm -rf feeds/packages/utils/watchcat
-#git clone --depth=1 https://github.com/openwrt/packages/tree/master/utils/watchcat feeds/packages/utils/watchcat
+# 3. 扩充物理寻址：将寻址范围从 128M (0x8000000) 改为 512M (0x20000000)
+# 注意：此命令会搜索所有匹配的 128M 地址并替换为 512M
+sed -i 's/0x8000000/0x20000000/g' target/linux/mediatek/dts/mt7981-360-t7-108M.dts
+
+# 4. 扩充逻辑分区：修改 UBI 分区寄存器定义，将 size 从 108M 对应的长度拉伸到 512M 对应的长度
+# 这里的 0x1ef00000 是给 UBI 预留约 495MB 的空间
+sed -i '/partition@1100000 {/,/};/ s/reg = <0x1100000 0x6900000>/reg = <0x1100000 0x1ef00000>/' target/linux/mediatek/dts/mt7981-360-t7-108M.dts
+
+# 5. 解除编译镜像大小限制：将原本 108M 的镜像限制放宽，防止编译程序报错
+sed -i 's/IMAGE_SIZE := 108M/IMAGE_SIZE := 500M/g' target/linux/mediatek/image/mt7981.mk
+
+# --- 512M Flash 适配结束 ---
