323e07337275cf4d83739669963b3b5aaca9f5fb
null <68135821+shoujidi@users.noreply.github.com>
2026-04-10 14:54:04 +0800

    Update 2410-6.6-diy-part2-6-1-pw2.sh

--- 2410-6.6-diy-part2-6-1-pw2.sh
+++ 2410-6.6-diy-part2-6-1-pw2.sh
@@ -6,12 +6,11 @@
 #
 
 # 1. 修改默认 IP (已保留你的设置)
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # 2. 修改主机名 (修正语法错误)
 # 使用 bash 的日期变量，确保写入文件的是最终生成的字符串，而不是 Makefile 脚本代码
-date_version=$(date +"%y%m%d")
-sed -i "s/hostname='ImmortalWrt'/hostname='360T7-PW2-$date_version'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
 
 # 3. 修改固件生成后的文件名 (增加日期前缀)
 # 这个在 include/image.mk 里可以使用 Makefile 语法，保留但优化了格式
