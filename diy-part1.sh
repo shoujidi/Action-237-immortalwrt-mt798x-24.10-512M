f4a1083860fb046dd122c1eaedc7ed22ab5bdef1
null <68135821+shoujidi@users.noreply.github.com>
2026-04-10 15:47:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,8 @@
 #
 
 # 1. 按照原作者要求，在 feeds 第一行插入最新的 Passwall 源码
-sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+#sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+#sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 
 # 2. 添加 OpenClash 官方源
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
